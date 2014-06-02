cd13a4a549272073f8963b4ede70c997713ca9a2
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-06-02 17:12:13 +0530

    setup all required files for package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Rotto-Links-Web-Scraper
+Rotto-Links-Scraper
 =======================
 
 A web crawler/scraper to find the broken links in the targeted seed url based on the keywords matched around that broken links. 
---
+++ setup.py
@@ -0,0 +1,85 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+import rottoscraper
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Rotto Links Scraper',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version=rottoscraper.__version__,
+
+    description='A Broken Links Scraper',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/KodeKracker/Rotto-Links-Scraper',
+
+    # Author details
+    author='Akshay Pratap Singh',
+    author_email='pratapakshay0@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='broken links web crawler scraper keywords search',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=[
+        'argparse==1.2.1',
+        'beautifulsoup4==4.3.2',
+        'requests==2.3.0'
+        'requests-cache==0.4.4'
+    ],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'rottoscraper=rottoscraper:main',
+        ],
+    },
+)
