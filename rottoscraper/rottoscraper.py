fc59cbe4cba73eac786c6cee123c55f87fec7f94
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-06-03 13:37:31 +0530

    edited main file

--- rottoscraper/rottoscraper.py
+++ rottoscraper/rottoscraper.py
@@ -1,57 +1,81 @@
+#! /usr/bin/python
 
-from bs4 import BeautifulSoup
-import urllib2
 import requests
+import urllib2
+from bs4 import BeautifulSoup
 from urlparse import urljoin
 
 
-def print_links(lnk):
-		for l in lnk:
-			print l
+def print_links(links):
+	"""Print all the links of a set"""
+	cnt = 1
+	for l in links:
+		print str(cnt)+') '+l
+		cnt += 1
+
+def get_object(url):
+	"""Return the requests object of a Url"""
+	headers = {'User-agent': 'Mozilla/5.0'}
+	r = requests.get(url,headers=headers)
+	return r
+
+def get_links(url):
+	"""Return the set of links from a particular Url"""
+	r = get_object(url)
+	soup = BeautifulSoup(r.text)
+	links = soup.find_all('a', href=True)
+	return links
+
+def get_status_code(url):
+	"""Return the status code of a given Url"""
+	r = get_object(url)
+	return r.status_code
+
+def start_crawler(seed_url):
+	"""Return the set of rotto links from a seed Url"""
+	bravo_links = []	# a set of all fine/unbroken links
+	rotto_links = []	# a set of all broken links 
+	visited_links = []	# a set of visited links
+	base_url = seed_url # base url 
+
+	links = get_links(seed_url)
+
+	for l in links:
+		url = l['href']
+		if not url.startswith('http'):
+			url = urljoin(base_url, url)
+		if url not in visited_links and url.startswith(base_url):
+			print 'Visiting Url : ', url
+			if ( get_status_code(url) == requests.codes.ok):
+				bravo_links.append(url)
+			else:
+				rotto_links.append(url)
+			visited_links.append(url)
+		else:
+			if url in visited_links:
+				print 'Already Visited :', url
+			else:
+				print 'External Link : ', url
+		print '\n'
+		
+
+	print '\n'
+	print 'Total Bravo Links : ', len(bravo_links)
+	print 'Total Rotto Links : ', len(rotto_links)
+	print '\n'
+	print 'List of Bravo Links : '
+	print_links(bravo_links)
+	print '\n'
+	print 'List of Rotto Links : '
+	print_links(rotto_links)
+	print '\n'
+
+def main():
+	"""Main function of the crawler"""
+	seed_url = raw_input("Enter the seed url: ")
+	print 'Crawler Starts..........\n'
+	start_crawler(seed_url)
+	print 'Crawler Stops........... \n'
 
 if __name__ == "__main__":
-	# Base Url :: Target site
-	base_url = "http://www.saurabh-kumar.com"
-	r = requests.get(base_url)
-	soup = BeautifulSoup(r.text)
-
-	# Extract all links
-	links = soup.find_all('a')
-
-	#Structure to store all Relevant Links
-	list_ok = []
-	list_notok = []
-	visited_link = []
-
-
-	print len(links)
-	header = {'User-agent': 'Mozilla/5.0'}
-	# Iterating all links in Page
-	for lk in links:
-		if lk.has_attr('href'):
-			link = lk['href']
-			url = urljoin(base_url,link)
-			#print 'url = ',url
-			if url not in visited_link and url.startswith(base_url):
-				print 'Visiting Url = ', url
-				r = requests.get(url,headers=header)
-				if (r.status_code == requests.codes.ok):
-					list_ok.append(url)
-				else:
-					list_notok.append(url)
-
-				visited_link.append(url)
-			else:
-				if url in visited_link:
-					print 'Already Visited.', url
-				else:
-					print 'External Link', url
-		else:
-			pass
-
-	# Display result
-	print "Ok link::"
-	print_links(list_ok)
-
-	print "Roto link::"
-	print_links(list_notok)
+	main()
