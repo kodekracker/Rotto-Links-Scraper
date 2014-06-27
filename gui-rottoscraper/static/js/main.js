6cbe7ae9919d15fd5016c5421b14510caf943548
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-06-27 12:35:49 +0530

    little modification in style.css

--- gui-rottoscraper/static/js/main.js
+++ gui-rottoscraper/static/js/main.js
@@ -2,10 +2,10 @@
 $(document).ready(function(){
 
 	$(".btn").click(function(e){
-		var $this   = $(this);
 		$(".steps").animate({"margin-left":"-=660px"},500, function(){
 			// callback action
 		});
+
+        e.preventDefault();
 	});
-	e.preventDefault();
 });
